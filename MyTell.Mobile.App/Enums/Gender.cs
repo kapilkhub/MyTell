28acec7c09a4c52d40cd7cc2c60b81d0a15e46d0
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-06-10 22:21:38 +0200

    commit for adding identity api

---
+++ MyTell.Mobile.App/Enums/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace MyTell.Mobile.App.Enums
+{
+	public enum Gender
+	{
+		Male=1, 
+		Female=2
+	}
+}
--- MyTell.Mobile.App/ViewModels/Identity/RegistrationViewModel.cs
+++ MyTell.Mobile.App/ViewModels/Identity/RegistrationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using MyTell.Mobile.App.Entities;
+using MyTell.Mobile.App.Enums;
 using MyTell.Mobile.App.Services;
 using MyTell.Mobile.App.ViewModels.Base;
 using System.ComponentModel.DataAnnotations;
@@ -14,10 +15,70 @@ namespace MyTell.Mobile.App.ViewModels.Identity
 
 		[ObservableProperty]
 		[Required]
+		[MinLength(3)]
+		[MaxLength(50)]
+		[NotifyDataErrorInfo]
+		private string _firstName = default!;
+
+		[ObservableProperty]
+		[MinLength(3)]
+		[MaxLength(100)]
+		[NotifyDataErrorInfo]
+		private string _middleName = default!;
+
+		[ObservableProperty]
+		[Required]
+		[MinLength(3)]
+		[MaxLength(50)]
+		[NotifyDataErrorInfo]
+		private string _lastName = default!;
+
+		[ObservableProperty]
+		[Required]
+		[MinLength(15)]
+		[MaxLength(15)]
+		[NotifyDataErrorInfo]
+		private string _emiratesId = default!;
+
+		[ObservableProperty]
+		[Required]
+		[NotifyDataErrorInfo]
+		private DateOnly _dateOfBirth = default!;
+
+		[ObservableProperty]
+		[Required]
+		[NotifyDataErrorInfo]
+		private Gender _gender = default!;
+
+
+		[ObservableProperty]
+		[Required]
 		[EmailAddress]
 		[MinLength(5)]
 		[NotifyDataErrorInfo]
 		private string _email = default!;
+
+		[ObservableProperty]
+		[Required]
+		[EmailAddress]
+		[MinLength(5)]
+		[NotifyDataErrorInfo]
+		private string _confirmedEmail = default!;
+
+		[ObservableProperty]
+		[Required]
+		[MinLength(12)]
+		[MaxLength(30)]
+		[NotifyDataErrorInfo]
+		private string _password = default!;
+
+		[ObservableProperty]
+		[Required]
+		[MinLength(12)]
+		[MaxLength(30)]
+		[NotifyDataErrorInfo]
+		private string _confirmPassword = default!;
+
 		public RegistrationViewModel(INavigationService navigationService)
 			: base(navigationService)
 		{
@@ -29,6 +90,14 @@ namespace MyTell.Mobile.App.ViewModels.Identity
 			{
 				Shell.Current.DisplayAlert("Data", "is present", "ok");
 			}
+		}
+		public override async Task InitializeAsync()
+		{
+			await IsBusyFor(async () =>
+			{
+				Email = "kk.jecrc@gmail.com";
+				await Task.FromResult(0);
+			});
 		}
 	}
 }
